b58aece6567ea0c75585f1c5ce0f07c7d795e7fe
User <User@Charas-MacBook-Air.local>
2022-02-14 14:21:47 +0100

    hello

--- homepage/homePage.svelte
+++ homepage/homePage.svelte
@@ -1,4 +1,115 @@
 <!--
-    
-
 -->
+
+
+<body>
+
+    <nav class="navbar">
+        <a href="Menu Bars" class= "rectangle-21"> </a>
+        <a href="#Find A Table" class = "rectangle-20" >FIND A TABLE</a>
+        <a href="#Order Online" class = "rectangle-19">ORDER ONLINE</a>
+        <a href="(06) 7984-3419" class= "rectangle-18">(06)7984-3419</a>
+    </nav> 
+    <style>
+
+        .table-rows-icon-1 {
+            width: 28px;
+            height: 28px;
+            background: #000000;
+        }
+
+
+        /*MENU ICON RECT1*/
+
+        .rectangle-21 {
+            width: 54px;
+            height: 50px;
+            padding: 8px 8px 8px 8px;
+            background: #ffffff;
+            border-color: #000000;
+            border-width: 1px;
+            border-style: solid;
+        }
+
+
+        /*MENU ICON RECT2*/
+
+        .rectangle-20 {
+            width: 128px;
+            height: 50px;
+            padding: 8px 8px 8px 8px;
+            background: #ffffff;
+            border-color: #000000;
+            border-width: 1px;
+            border-style: solid;
+        }
+
+
+        /*TEXT RECT2 MENU ICON*/
+
+        .paragraph-16 {
+            width: 98px;
+            height: 19px;
+            color: #798da3;
+            font-family: "Open Sans";
+            font-weight: 400;
+            font-size: 16px;
+            line-height: 1.1;
+            text-align: center;
+        }
+
+
+        /*MENU RECT3*/
+
+        .rectangle-19 {
+            width: 120px;
+            height: 50px;
+            padding: 8px 8px 8px 8px;
+            background: #ffffff;
+            border-color: #000000;
+            border-width: 1px;
+            border-style: solid;
+        }
+
+
+        /*TEXT RECT3*/
+
+        .paragraph-17 {
+            width: 98px;
+            height: 19px;
+            color: #798da3;
+            font-family: "Open Sans";
+            font-weight: 400;
+            font-size: 16px;
+            line-height: 1.1;
+            text-align: center;
+        }
+
+
+        /*MENU RECT4*/
+
+        .rectangle-18 {
+            width: 126px;
+            height: 50px;
+            padding: 8px 8px 8px 8px;
+            background: #ffffff;
+            border-color: #000000;
+            border-width: 1px;
+            border-style: solid;
+        }
+
+
+        /*TEXT RECT4*/
+
+        .paragraph-27 {
+            width: 98px;
+            height: 19px;
+            color: #798da3;
+            font-family: "Open Sans";
+            font-weight: 400;
+            font-size: 16px;
+            line-height: 1.1;
+            text-align: center;
+        }
+    </style>
+</body>
